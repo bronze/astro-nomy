eda8d0877bdfc598992e66f57a9c506d3b30c212
Carlos Bronze <170220+bronze@users.noreply.github.com>
2025-06-13 18:42:42 -0300

    post mapping is borked

--- src/lib/fetchers.ts
+++ src/lib/fetchers.ts
@@ -1,18 +1,14 @@
-import { getCollection } from "astro:content";
+import {getCollection} from "astro:content";
 
 export async function getCategories() {
   const posts = await getCollection("blog");
-  const categories = [
-    ...new Set(posts.map((post) => post.data.category).flat()),
-  ];
+  const categories = [...new Set(posts.map((post) => post.data.category).flat())];
 
   return categories;
 }
 
 export async function getPosts() {
-  const posts = (await getCollection("blog")).sort(
-    (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
-  );
+  const posts = (await getCollection("blog")).sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
 
   return posts;
 }
@@ -26,9 +22,7 @@ export async function getPostsByCategory(category: string) {
 }
 
 export async function getGuides() {
-  const guides = (await getCollection("guides"))
-    .filter((guide) => guide.data.published)
-    .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
+  const guides = (await getCollection("guides")).filter((guide) => guide.data.published).sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
 
   return guides;
 }
