c9f64e29aacfbc9f9a9f54eba667c681892aa472
Carlos Bronze <170220+bronze@users.noreply.github.com>
2025-06-13 18:15:19 -0300

    tailwind config and old menu

--- src/components/main-navigation-menu.tsx
+++ src/components/main-navigation-menu.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
-import { cn } from "@/lib/utils";
-import { Icons } from "@/icons";
+import {cn} from "@/lib/utils";
+import {Icons} from "@/icons";
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -10,35 +10,15 @@ import {
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
-import { Badge } from "@/components/ui/badge";
-import { navMenuConfig } from "@/config/nav-menu";
-import type { MenuItem } from "@/types";
+import {Badge} from "@/components/ui/badge";
+import {navMenuConfig} from "@/config/nav-menu";
+import type {MenuItem} from "@/types";
 
 const links = navMenuConfig.links;
 const pages = navMenuConfig.pagesNav[0];
 const examples = navMenuConfig.examplesNav[0];
 
 export function MainNavigationMenu() {
-  // Detect dark mode
-  const [isDarkMode, setIsDarkMode] = React.useState(false);
-  
-  React.useEffect(() => {
-    // Check initial dark mode
-    setIsDarkMode(document.documentElement.classList.contains('dark'));
-    
-    // Watch for theme changes
-    const observer = new MutationObserver((mutations) => {
-      mutations.forEach((mutation) => {
-        if (mutation.attributeName === 'class') {
-          setIsDarkMode(document.documentElement.classList.contains('dark'));
-        }
-      });
-    });
-    
-    observer.observe(document.documentElement, { attributes: true });
-    
-    return () => observer.disconnect();
-  }, []);
   return (
     <NavigationMenu>
       <NavigationMenuList>
@@ -72,17 +52,8 @@ export function MainNavigationMenu() {
         <NavigationMenuItem>
           <NavigationMenuTrigger>{pages.title}</NavigationMenuTrigger>
           <NavigationMenuContent>
-            <ul 
-              className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]" 
-              style={{ 
-                backgroundColor: isDarkMode ? "#171717" : "#ffffff",
-                borderColor: isDarkMode ? "#262626" : "#e5e5e5",
-                boxShadow: "0 1px 3px rgba(0, 0, 0, 0.1)"
-              }}
-            >
-              {pages.items?.map((page) => (
-                <ListItem key={page.title} {...page} />
-              ))}
+            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+              {pages.items?.map((page) => <ListItem key={page.title} {...page} />)}
             </ul>
           </NavigationMenuContent>
         </NavigationMenuItem>
@@ -90,17 +61,8 @@ export function MainNavigationMenu() {
         <NavigationMenuItem>
           <NavigationMenuTrigger>{examples.title}</NavigationMenuTrigger>
           <NavigationMenuContent>
-            <ul 
-              className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]" 
-              style={{ 
-                backgroundColor: isDarkMode ? "#171717" : "#ffffff",
-                borderColor: isDarkMode ? "#262626" : "#e5e5e5",
-                boxShadow: "0 1px 3px rgba(0, 0, 0, 0.1)"
-              }}
-            >
-              {examples.items?.map((example) => (
-                <ListItem key={example.title} {...example} />
-              ))}
+            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+              {examples.items?.map((example) => <ListItem key={example.title} {...example} />)}
             </ul>
           </NavigationMenuContent>
         </NavigationMenuItem>
@@ -108,12 +70,7 @@ export function MainNavigationMenu() {
         {links ? (
           <NavigationMenuItem>
             {links.map((link) => (
-              <a
-                key={link.href}
-                href={link.href}
-                className={navigationMenuTriggerStyle()}
-                {...(link.forceReload ? { "data-astro-reload": true } : {})}
-              >
+              <a key={link.href} href={link.href} className={navigationMenuTriggerStyle()} {...(link.forceReload ? {"data-astro-reload": true} : {})}>
                 {link.title}
               </a>
             ))}
@@ -124,15 +81,7 @@ export function MainNavigationMenu() {
   );
 }
 
-const ListItem: React.FC<MenuItem> = ({
-  title,
-  href,
-  description,
-  launched,
-  disabled,
-  external,
-  forceReload,
-}) => {
+const ListItem: React.FC<MenuItem> = ({title, href, description, launched, disabled, external, forceReload}) => {
   const target = external ? "_blank" : undefined;
 
   return (
@@ -140,37 +89,25 @@ const ListItem: React.FC<MenuItem> = ({
       <a
         target={target}
         href={disabled ? undefined : href}
-        {...(forceReload ? { "data-astro-reload": true } : {})}
+        {...(forceReload ? {"data-astro-reload": true} : {})}
         className={cn(
-          "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-muted hover:text-foreground focus:bg-muted focus:text-foreground",
-          disabled
-            ? "pointer-events-none opacity-60"
-            : "pointer-events-auto opacity-100"
-        )}
-      >
+          "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+          disabled ? "text-muted-foreground hover:bg-transparent hover:text-muted-foreground" : ""
+        )}>
         <div className="flex items-center text-sm font-medium leading-none">
           <span className="mr-2">{title}</span>
           {disabled ? (
-            <Badge
-              variant="secondary"
-              radius="sm"
-              className="h-5 px-1.5 text-xs font-medium"
-            >
+            <Badge variant="secondary" radius="sm" className="h-5 px-1.5 text-xs font-medium">
               SOON
             </Badge>
           ) : null}
           {launched ? (
-            <Badge
-              radius="sm"
-              className="h-5 px-1.5 text-xs font-medium bg-[#ebf5ff] hover:bg-[#ebf5ff] text-[#0068d6]"
-            >
+            <Badge radius="sm" className="h-5 px-1.5 text-xs font-medium bg-[#ebf5ff] hover:bg-[#ebf5ff] text-[#0068d6]">
               NEW
             </Badge>
           ) : null}
         </div>
-        <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-          {description}
-        </p>
+        <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">{description}</p>
       </a>
     </li>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,74 +1,88 @@
-import type { Config } from 'tailwindcss'
-import defaultTheme from 'tailwindcss/defaultTheme'
-import typography from '@tailwindcss/typography'
+import type {Config} from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
+import typography from "@tailwindcss/typography";
+import animate from "tailwindcss-animate";
 
 export default {
-  darkMode: 'class',
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     container: {
       center: true,
-      padding: '1rem',
+      padding: "1rem",
       screens: {
-        '2xl': '1400px',
+        "2xl": "1400px",
       },
     },
     extend: {
       fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
-        heading: ['CalSans Semibold', ...defaultTheme.fontFamily.sans],
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        heading: ["CalSans Semibold", ...defaultTheme.fontFamily.sans],
       },
       height: {
-        18: '4.5rem',
+        18: "4.5rem",
       },
       spacing: {
-        18: '4.5rem',
+        18: "4.5rem",
       },
       colors: {
-        border: 'var(--border)',
-        input: 'var(--input)',
-        ring: 'var(--ring)',
-        background: 'var(--background)',
-        'background-secondary': 'var(--background-secondary)',
-        foreground: 'var(--foreground)',
-        'foreground-secondary': 'var(--foreground-secondary)',
-        primary: 'var(--primary)',
-        'primary-foreground': 'var(--primary-foreground)',
-        secondary: 'var(--secondary)',
-        'secondary-foreground': 'var(--secondary-foreground)',
-        destructive: 'var(--destructive)',
-        'destructive-foreground': 'var(--destructive-foreground)',
-        muted: 'var(--muted)',
-        'muted-foreground': 'var(--muted-foreground)',
-        accent: 'var(--accent)',
-        'accent-foreground': 'var(--accent-foreground)',
-        popover: 'var(--popover)',
-        'popover-foreground': 'var(--popover-foreground)',
-        card: 'var(--card)',
-        'card-foreground': 'var(--card-foreground)',
+        border: "var(--border)",
+        input: "var(--input)",
+        ring: "var(--ring)",
+        background: {
+          DEFAULT: "hsl(var(--background))",
+          200: "hsl(var(--background-200))",
+        },
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsla(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
       },
       borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
-        'accordion-down': {
-          from: { height: '0' },
-          to: { height: 'var(--radix-accordion-content-height)' },
+        "accordion-down": {
+          from: {height: "0"},
+          to: {height: "var(--radix-accordion-content-height)"},
         },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' },
+        "accordion-up": {
+          from: {height: "var(--radix-accordion-content-height)"},
+          to: {height: "0"},
         },
       },
       animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [
-    typography
-  ],
-} satisfies Config
+  plugins: [animate, typography],
+} satisfies Config;
